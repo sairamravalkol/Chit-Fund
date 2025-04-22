b7585413b4117bb9b1372f25ec83da0055c366da
Sairam <sairam.ravulakolu@gmail.com>
2025-04-22 12:03:50 +0530

    Artifact created

---
+++ Chit-fund/src/test/java/com/jbsvr/Chit_fund/ChitFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbsvr.Chit_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
